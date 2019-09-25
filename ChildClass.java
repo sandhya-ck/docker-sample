80055a0af8224a88b8e976550cdfdad9d816e0bc
sandhya-ck <55776374+sandhya-ck@users.noreply.github.com>
2019-09-25 09:47:40 +0530

    Add files via upload

---
+++ ChildClass.java
@@ -0,0 +1,25 @@
+package com.mindtree.enggcamp.inheritance;
+
+
+	public class ChildClass extends ParentClass{
+
+		String name;
+
+		public ChildClass()
+		{
+		System.out.println("This is a child class constructor");
+		}
+
+		public String getName() {
+		return name;
+		}
+
+		public void setName(String name) {
+		this.name = name;
+		}
+
+
+		}
+
+
+		
